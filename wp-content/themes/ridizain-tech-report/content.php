da6f73387e7c179562a5757be1fa89a173eb54b7
ljfranklin <lylejfranklin@gmail.com>
2014-03-20 16:55:36 -0700

    Fixed paper display on home page

--- wp-content/themes/ridizain-tech-report/content.php
+++ wp-content/themes/ridizain-tech-report/content.php
@@ -21,25 +21,6 @@
 				the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
 			endif;
 		?>
-
-		<div class="entry-meta">
-			<?php if ( in_array( 'category', get_object_taxonomies( get_post_type() ) ) && ridizain_categorized_blog() ) : ?>
-		
-			<span class="cat-links"><?php echo get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'ridizain' ) ); ?></span>
-		<?php
-			endif;
-				if ( 'post' == get_post_type() )
-					ridizain_posted_on();
-
-				if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) :
-			?>
-			<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'ridizain' ), __( '1 Comment', 'ridizain' ), __( '% Comments', 'ridizain' ) ); ?></span>
-			<?php
-				endif;
-
-				edit_post_link( __( 'Edit', 'ridizain' ), '<span class="edit-link">', '</span>' );
-			?>
-		</div><!-- .entry-meta -->
 		
 		<table class="paper_main_data">
 			<tbody>
@@ -58,7 +39,7 @@
 	
 	
 
-	<?php if ( is_search() || is_archive() ) : ?>
+	<?php if ( is_search() || is_archive() || is_home() ) : ?>
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 		<p class="read-more button"><a href="<?php echo esc_url( get_permalink() ); ?>"><?php _e( 'Read More &raquo;', 'ridizain' ); ?></a></p>
