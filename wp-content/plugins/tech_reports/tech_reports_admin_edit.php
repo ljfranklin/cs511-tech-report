253ec0d8618a3435dc9efe864326960621caf5e3
ljfranklin <lylejfranklin@gmail.com>
2014-04-08 09:31:48 -0700

    Added quick implementation of typeahead

--- wp-content/plugins/tech_reports/tech_reports_admin_edit.php
+++ wp-content/plugins/tech_reports/tech_reports_admin_edit.php
@@ -50,6 +50,64 @@
     };
 ?>
 
+<?php $plugin_url = plugin_dir_url( __FILE__ ); ?>
+
+<link href="<?php echo $plugin_url; ?>scripts/typeahead.css" rel="stylesheet" type="text/css">
+
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="<?php echo $plugin_url; ?>scripts/typeahead.bundle.js"></script>
+<script>
+
+$(document).ready(function() {
+	var substringMatcher = function(strs) {
+	  return function findMatches(q, cb) {
+		var matches, substringRegex;
+	 
+		// an array that will be populated with substring matches
+		matches = [];
+	 
+		// regex used to determine if a string contains the substring `q`
+		substrRegex = new RegExp(q, 'i');
+	 
+		// iterate through the pool of strings and for any string that
+		// contains the substring `q`, add it to the `matches` array
+		$.each(strs, function(i, str) {
+		  if (substrRegex.test(str)) {
+		    // the typeahead jQuery plugin expects suggestions to a
+		    // JavaScript object, refer to typeahead docs for more info
+		    matches.push({ value: str });
+		  }
+		});
+	 	
+		cb(matches);
+	  };
+	};
+	 
+	var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+	  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+	  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+	  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+	  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+	  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+	  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+	  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+	  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+	];
+	console.log($('.typeahead').size());
+	$('.typeahead').typeahead({
+	  hint: true,
+	  highlight: true,
+	  minLength: 1
+	},
+	{
+	  name: 'states',
+	  displayKey: 'value',
+	  source: substringMatcher(states)
+	});
+});
+
+</script>
+
 <?php echo implode($tech_report->get_all_authors()); ?>
 
 <div class="wrap">
@@ -76,6 +134,7 @@
 					</th>
 					<td>
 						<input type="text" id="paper_author" name="paper_author" size="30" value="<?php echo $get_existing_value('author') ?>" required/>
+					 	<input class="typeahead" type="text" placeholder="Search Author Names">
 					</td>
 				</tr>
 				<tr>
