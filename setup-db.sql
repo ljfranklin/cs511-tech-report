17bdbab50a718e2ef2782f87d5a0c6cf4e2366e1
ljfranklin <lylejfranklin@gmail.com>
2014-04-25 15:20:13 -0700

    Updated styling on readme

--- setup-db.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE wordpress;
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY 'your-password-here';
-
-CREATE DATABASE tech_papers;
-GRANT ALL PRIVILEGES ON tech_papers.* TO 'wordpress'@'localhost';
