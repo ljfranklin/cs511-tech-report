02be8de40df4cb5084222abc3e55adfa6b1cac7e
ljfranklin <lylejfranklin@gmail.com>
2014-04-25 15:24:34 -0700

    Readded config files

---
+++ setup-db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE wordpress;
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY 'wp1234';
+
+CREATE DATABASE tech_papers;
+GRANT ALL PRIVILEGES ON tech_papers.* TO 'wordpress'@'localhost';
